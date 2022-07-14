3b6df32b018fdfaea1da94af95807ceed8c8e580
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2022-07-14 18:29:40 +0530

    save to server, load from server working

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 
+const cors = require('cors');
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
+
 app.use(
   express.urlencoded({
     extended: true,
