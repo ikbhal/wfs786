143e75752e39e6c3da63ac72eb8e621cb3615a33
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2022-07-15 12:09:06 +0530

    nebula docker compose fail, reading lists of graph database

--- tried/not working/neo4j_connect.js
+++ tried/not working/neo4j_connect.js
@@ -3,6 +3,9 @@ const neo4j = require('neo4j-driver').v1;
 const auth = neo4j.auth.basic('neo4j', 'omega16');
 const driver = neo4j.driver(database, auth);
 
+console.log("START");
 driver.onError = (error) => {
   console.log('Driver instantiation failed', error);
 }
+
+console.log("END");
