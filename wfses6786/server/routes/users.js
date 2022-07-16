85041cac36ac50c9d245646bcf9dde29dd9799cb
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2022-07-16 20:10:36 +0530

    not woking even hello express es6, cross_env issue , fixed, earlier, but showing again

--- wfses6786/server/routes/users.js
+++ wfses6786/server/routes/users.js
@@ -1,10 +1,29 @@
 // var express = require('express');
 import express from 'express';
+
+
+
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.get('redis_test', function(req,res, next){
+
+  const client = createClient();
+
+  client.on('error', (err) => console.log('Redis Client Error', err));
+
+  await client.connect();
+
+  await client.set('allah', '786');
+  const value = await client.get('allah');
+
+  console.log("value retrieved from redis ", value);
+
+  res.send("value retrieved from redis "+ value);
 });
 
 // module.exports = router;
